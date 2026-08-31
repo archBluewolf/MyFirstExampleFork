8dda7e87870d5e4681723857d6ffb5a7babef960
Harrison Stidham <hjstidham@csuchico.edu>
2026-08-31 11:38:36 -0700

    fixed divide by zero problem, close #61

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    y != 0 ? cout << "Division: " << x / y << endl : cout << "Dividing by zero is not a number." << endl;
+    if (y != 0) cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
