91150a87507e2b8b0c2bd32624dbbb3ee934b9bb
Harrison Stidham <hjstidham@csuchico.edu>
2026-08-31 15:13:41 -0700

    int32 data type and number range output message, close #340

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ using std::cout;
 int main()
 {
     cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    cout << "(Limited to whole numbers between -2,147,483,648 and 2,147,483,647)\n";
     cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int32_t x,y;
 
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
